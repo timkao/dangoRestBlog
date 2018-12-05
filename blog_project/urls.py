babc1e83d5e2454b00bda1148071b943a1df33dc
Tim Kao <pakuya.kao@gmail.com>
2018-12-04 20:47:14 -0500

    basic documentation

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,11 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+API_TITLE = 'Blog API'
+API_DESCRIPTION = 'A Web API for creating and editing blog posts'
+schema_view = get_schema_view(title='Blog API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
-    path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls'))
+    path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('schema/', schema_view),
+    path('docs/', include_docs_urls(
+        title=API_TITLE,
+        description=API_DESCRIPTION
+        ))
 ]
