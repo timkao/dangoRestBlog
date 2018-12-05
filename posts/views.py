a51d1ac22c143cf73f17c5ac558268a05a361dec
Tim Kao <pakuya.kao@gmail.com>
2018-12-04 20:15:40 -0500

    configure user endpoints

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
+from django.contrib.auth import get_user_model
 
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
@@ -15,3 +16,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = (IsAuthorOrReadOnly,)
   queryset = Post.objects.all()
   serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+  queryset = get_user_model().objects.all()
+  serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = get_user_model().objects.all()
+  serializer_class = UserSerializer
